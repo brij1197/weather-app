8236f26036ce6bac49c32b538ccce2186a5aed05
Brij B <brijbhatia997@gmail.com>
2025-01-10 06:14:22 -0800

    Changed Env

--- api/base.ts
+++ api/base.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://api.weatherapi.com/v1",
   params: {
-    key: process.env.EXPO_PUBLIC_API_KEY,
+    key: process.env.WEATHER_API_KEY,
   },
 });
